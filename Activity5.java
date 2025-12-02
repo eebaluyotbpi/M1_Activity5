f4312fc6008132198ceeb32b58417b931d2a91ae
Ericson E. Baluyot <eebaluyot@headoffice.corp.bpi.com.ph>
2025-12-02 16:19:05 +0800

    initialize

---
+++ Activity5.java
@@ -0,0 +1,17 @@
+package bpi.project1.hello;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int add = 0;
+        int x = 1;
+
+        while (x <= 50) {
+            add += x;
+            x++;
+        }
+        System.out.println("Sum = " + add);
+	}
+
+}
